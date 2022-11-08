ad97365d349ecb431753a4bed6e5c05e0531b316
Sunil Pavar <55226366+sunilpavar98@users.noreply.github.com>
2022-11-08 18:46:17 +0530

    Coding is so easy
    Make Watch Calculator Using HTML CSS And JS by Coding is so easy
    
    Channal Link : -https://www.youtube.com/@codingissoeasy

---
+++ script.js
@@ -0,0 +1,36 @@
+var numBtns = document.getElementsByClassName('btn_num')
+var operatorBtns = document.getElementsByClassName('btn_operator')
+
+var btnDecimal = document.querySelector('.calc_decimal')
+var btnClear = document.getElementById('calc_Clear')
+var display = document.getElementById('display')
+
+var displayVal = '0'
+var pendingVal ;
+var evalStringArry = []
+
+btnClear.addEventListener('click', ()=>{
+    
+})
+
+btnDecimal.addEventListener('click', ()=>{
+
+})
+
+var UpdateDisplyVal = (clickObj) =>{
+    
+}
+
+for(var i=0; i < numBtns.length;i++){
+    
+}
+
+var PerformOperation = (clickObj) =>{
+    
+}
+
+
+
+for(var i=0; i < operatorBtns.length;i++){
+
+}
